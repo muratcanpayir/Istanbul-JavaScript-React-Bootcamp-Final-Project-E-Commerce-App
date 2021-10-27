9a65028c3a38a8198245b484dbc416efe9ba9fef
Muratcan Payir <muratcanpayir@gmail.com>
2021-10-27 21:02:43 +0100

    Signup and Login form validations added

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -2,27 +2,38 @@ import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
 import { useHistory } from "react-router";
+import REQUEST_STATUS from "../../helpers/constants";
 import { postLogin } from "../../redux/actions/loginAction";
+import {useForm} from "react-hook-form";
 import "../Signup/Signup.scss";
+import "./Login.scss";
 
 function Signup() {
-  const [email, setEmail] = useState("muratcanpayir@gmail.com");
-  const [password, setPassword] = useState("123465798");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const history = useHistory();
   const dispatch = useDispatch();
   const token = useSelector((state) => state.login);
-  console.log(token.data)
-  const access=()=>{
-    dispatch(postLogin(email,password))
-  }
-  useEffect(()=>{
-    localStorage.setItem("access_token",token.data);
-    
-  },[access])
+  console.log(token.data);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const access = () => {
+    dispatch(postLogin(email, password));
+  };
+  useEffect(() => {
+    if (token.status === REQUEST_STATUS.SUCCESS) {
+      localStorage.setItem("access_token", token.data);
+      localStorage.setItem("email",email);
+      window.location.href="/";
+    }
+  }, [token]);
   return (
     <div className="signup-container">
       <div className="form-container">
-        <form>
+        <form onSubmit={handleSubmit(access)}>
           <p className="signup-title">Login</p>
           <p className="signup-go-login">
             Login to our website to use this form. If you don't have an account{" "}
@@ -37,14 +48,26 @@ function Signup() {
           <input
             className="input-light"
             placeholder="E-Mail"
+            id={errors.email && "login-email-error"}
+            {...register("email", {
+              required: true,
+              pattern: /\S+@\S+\.\S+/,
+            })}
             onChange={(e) => setEmail(e.target.value)}
           />
           <input
             className="input-light"
             placeholder="Password"
+            type="password"
+            id={errors.email && "login-password-error"}
+            {...register("password", {
+              required: true,
+              minLength: 8,
+              maxLength: 20,
+            })}
             onChange={(e) => setPassword(e.target.value)}
           />
-          <button onClick={access} className="button-light" id="signup-button">
+          <button className="button-light" id="signup-button">
             LOGIN
           </button>
         </form>
--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -1,20 +1,72 @@
-import React from "react";
+import React, { useState,useEffect } from "react";
 import { useHistory } from "react-router";
+import { useForm } from "react-hook-form";
 import "./Signup.scss";
+import { useDispatch } from "react-redux";
+import {postLogin} from "../../redux/actions/loginAction";
+import { useSelector } from "react-redux";
+import REQUEST_STATUS from "../../helpers/constants";
 
 function Signup() {
-  const history=useHistory();
+  const dispatch=useDispatch();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const history = useHistory();
+  const token=useSelector(state=>state.login);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const access = () => {
+    dispatch(postLogin(email, password));
+  };
+  useEffect(() => {
+    if (token.status === REQUEST_STATUS.SUCCESS) {
+      localStorage.setItem("access_token", token.data);
+      localStorage.setItem("email",email)
+      window.location.href="/";
+    }
+  }, [token]);
   return (
     <div className="signup-container">
       <div className="form-container">
-        <form>
+        <form onSubmit={handleSubmit(access)}>
           <p className="signup-title">Sign Up</p>
-          <p className="signup-go-login">Create a free account to buy amazing product. Already have an account? <span onClick={()=>{
-            history.push("/login");
-          }}>Login Here!</span></p>
-          <input className="input-light" placeholder="E-Mail"/>
-          <input className="input-light" placeholder="Password" />
-          <button className="button-light" id="signup-button">SIGN UP</button>
+          <p className="signup-go-login">
+            Create a free account to buy amazing product. Already have an
+            account?{" "}
+            <span
+              onClick={() => {
+                history.push("/login");
+              }}
+            >
+              Login Here!
+            </span>
+          </p>
+          <input
+            className="input-light"
+            placeholder="E-Mail"
+            {...register("email", {
+              required: true,
+              pattern: /\S+@\S+\.\S+/,
+            })}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            className="input-light"
+            placeholder="Password"
+            {...register("password", {
+              required: true,
+              minLength: 8,
+              maxLength: 20,
+            })}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button className="button-light" id="signup-button">
+            SIGN UP
+          </button>
         </form>
       </div>
     </div>
