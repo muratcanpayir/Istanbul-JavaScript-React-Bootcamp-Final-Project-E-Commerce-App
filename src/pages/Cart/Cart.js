02d354eca041b69d52ddbf01be914556a50224c1
Muratcan Payir <muratcanpayir@gmail.com>
2021-10-27 15:06:24 +0100

    Cart page delete button icon added

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -3,7 +3,11 @@ import { useDispatch, useSelector } from "react-redux";
 import Header from "../../components/Header/Header";
 import REQUEST_STATUS from "../../helpers/constants";
 import { getCart, resetCart } from "../../redux/actions/cartAction";
-import { deleteFromCart, resetDeleteOffer } from "../../redux/actions/deleteFromCartAction";
+import { RiDeleteBinLine } from "react-icons/ri";
+import {
+  deleteFromCart,
+  resetDeleteOffer,
+} from "../../redux/actions/deleteFromCartAction";
 import "./Cart.scss";
 
 function Cart() {
@@ -18,47 +22,48 @@ function Cart() {
     if (cart.status === REQUEST_STATUS.SUCCESS) {
       setTotalPrice(0);
       cart.data.map((item) => {
-        setTotalPrice(prev=>prev+Number(item.price));    
+        setTotalPrice((prev) => prev + Number(item.price));
       });
     }
   }, [cart]);
-  const deleteProductFromCart=(id)=>{
-    dispatch(deleteFromCart(id))
-  }
-  const deleteState=useSelector(state=>state.deleteFromCart)
-  useEffect(()=>{
-    if(deleteState.status===REQUEST_STATUS.SUCCESS){
-      window.location.href="/cart";
+  const deleteProductFromCart = (id) => {
+    dispatch(deleteFromCart(id));
+  };
+  const deleteState = useSelector((state) => state.deleteFromCart);
+  useEffect(() => {
+    if (deleteState.status === REQUEST_STATUS.SUCCESS) {
+      window.location.href = "/cart";
     }
-  },[deleteState])
+  }, [deleteState]);
 
   return (
     <>
-    <Header />
-    <div className="cart-container">
-      <div className="cart-wrapper">
-      burasi cart
-      {cart.data.map((item) => (
-        <div className="cart-items" key={item.id}>
-          <div className="cart-image">
-            <img src={item.imageUrl} alt={item.title} />
+      <Header />
+      <div className="cart-container">
+        <div className="cart-wrapper">
+          {cart.data.map((item) => (
+            <div className="cart-items" key={item.id}>
+              <div className="cart-image">
+                <img src={item.imageUrl} alt={item.title} />
+              </div>
+              <div className="cart-items-right-side">
+                <div className="cart-items-info">
+                  <div className="cart-title">
+                    <p>{item.title}</p>
+                  </div>
+                  <div className="cart-price">
+                    <p>{item.price} TL</p>
+                  </div>
+                </div>
+                <button onClick={() => deleteProductFromCart(item.id)}>
+                  <RiDeleteBinLine size={"24px"} />
+                </button>
+              </div>
             </div>
-            <div className="cart-items-right-side">
-            <div className="cart-items-info">
-          <div className="cart-title">{item.title}</div>
-          <div className="cart-price">{item.price}</div>
-          </div>
-          <button
-            onClick={()=>deleteProductFromCart(item.id)}
-          >
-            delete
-          </button>
-          </div>
+          ))}
+          <div>{totalPrice}</div>
         </div>
-      ))}
-      <div>{totalPrice}</div>
-    </div>
-    </div>
+      </div>
     </>
   );
 }
