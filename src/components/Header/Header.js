dda6a9af5efab5e4c64b73a62ad8a2349739ad81
Muratcan Payir <muratcanpayir@gmail.com>
2021-10-27 15:44:21 +0100

    Header Subtotal Context Fixes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import { useHistory } from "react-router";
 import "./Header.scss";
 import useCart from "../../hooks/useCart";
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import REQUEST_STATUS from "../../helpers/constants";
+import { getCart } from "../../redux/actions/cartAction";
 
 function Header() {
-  const { totalPrice } = useCart();
+  const { totalPrice,setTotalPrice } = useCart();
+  const dispatch=useDispatch()
+  useEffect(()=>{
+    dispatch(getCart());
+  },[])
+  const cart=useSelector(state=>state.cart)
+  useEffect(() => {
+    if (cart.status === REQUEST_STATUS.SUCCESS) {
+      setTotalPrice(0);
+      cart.data.map((item) => {
+        setTotalPrice((prev) => prev + Number(item.price));
+      });
+    }
+  }, [cart]);
+  
+  console.log(cart);
+  
   const history = useHistory();
   return (
     <header>
